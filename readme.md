e711a460a43c20fc4e5b7f290c69146f023fc0ef
felixmaker <1162850692@qq.com>
2023-06-19 17:21:46 +0800

    初始提交

---
+++ readme.md
@@ -0,0 +1,5 @@
+# 本仓库为 scoop-felixmaker 的镜像仓库
+
+本仓库只是为了国内用户加速使用下载使用
+
+提交 issue 请前往 https://github.com/felixmaker/scoop-felixmaker
---
+++ update.ps1
@@ -0,0 +1,28 @@
+# 开启调试
+# Set-PSDebug -Trace 1
+
+# 删除已有的文件
+Remove-Item -Path .\bucket -Recurse -Force
+
+# 将克隆的最新的文件拷贝到待处理的文件夹
+New-Item -ItemType "directory" -Path ".\bucket"
+
+# 仓库
+Copy-Item -Path ".\scoop-felixmaker\bucket\*" -Destination ".\bucket" -Recurse -Force
+
+# 复制完后，删掉克隆的文件夹
+Remove-Item -Path .\scoop-felixmaker -Recurse -Force
+
+# install.ps1
+(Invoke-RestMethod https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1).Replace('"https://github.com', '"https://ghproxy.com/https://github.com') | Set-Content -Path .\install.ps1
+# 本地调试时使用
+# (Invoke-RestMethod https://ghproxy.com/https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1).Replace('"https://github.com', '"https://ghproxy.com/https://github.com') | Set-Content -Path .\install.ps1
+
+Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
+    # GitHub Releases
+    (Get-Content $_.FullName) -replace '(github\.com/.+/releases/download)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
+
+    # GitHub Raw
+    (Get-Content $_.FullName) -replace '(raw\.githubusercontent\.com)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
+    (Get-Content $_.FullName) -replace '(github\.com/.+/raw)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
+}
