a053cbb74a97d130e0f5b57e7c7b27fc67beee34
felixmaker <7445790+felixmaker@users.noreply.github.com>
2023-11-04 17:17:03 +0800

    Change mirror to gh-proxy

--- update.ps1
+++ update.ps1
@@ -9,9 +9,9 @@ Remove-Item -Path .\scoop-felixmaker -Recurse -Force
 
 Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
     # GitHub Releases
-    (Get-Content $_.FullName) -replace '(github\.com/.+/releases/download)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
+    (Get-Content $_.FullName) -replace '(github\.com/.+/releases/download)', 'gh-proxy.com/https://$1' | Set-Content -Path $_.FullName
 
     # GitHub Raw
-    (Get-Content $_.FullName) -replace '(raw\.githubusercontent\.com)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
-    (Get-Content $_.FullName) -replace '(github\.com/.+/raw)', 'ghproxy.com/https://$1' | Set-Content -Path $_.FullName
+    (Get-Content $_.FullName) -replace '(raw\.githubusercontent\.com)', 'gh-proxy.com/https://$1' | Set-Content -Path $_.FullName
+    (Get-Content $_.FullName) -replace '(github\.com/.+/raw)', 'gh-proxy.com/https://$1' | Set-Content -Path $_.FullName
 }
